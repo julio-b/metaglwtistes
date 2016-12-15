2790676eb2b431dc200e46a5c1c5b74d9d79df98
Julio B <tzoulio13@gmail.com>
2016-12-15 03:07:59 +0200

    small fix

--- A/Metaglotistes1.cpp
+++ A/Metaglotistes1.cpp
@@ -7,39 +7,55 @@
 using namespace std;
 
 int main(){
-    string fname;
-    string log="";
-	bool itsFine = true;
+	string fname;
+	string log="";
+	bool in_String = false;
+	bool escape_char = false;
 	char c;
 	stack<char> z;
 	cout<<"Give the name of the file\n";
 	cin>>fname;
 	fstream fs;
-    fs.open(fname.c_str());
+	fs.open(fname.c_str());
 	while(fs.get(c)){
-            if(c=='\"'){
-                fs.ignore(numeric_limits<streamsize>::max(),'\"');
-            }
-            if(c=='\'')
-                fs.ignore(numeric_limits<streamsize>::max(),'\'');            
-        if(c=='('){
-            z.push(c);
-        }
-        if(c==')'){
-            if(z.empty())
-                itsFine=false;
-            else
-                z.pop();
-        }
+		cout<<c;
+		if(in_String){
+			if(!escape_char){
+				switch(c){
+				 case '"':
+					in_String=false;
+					break;
+				 case '\\':
+					escape_char=true;
+					break;
+				}
+			}else{
+				escape_char=false;
+			}
+		}else{
+			switch(c){
+			 case '(':
+				z.push(c);
+				break;
+			 case ')':
+				if(z.empty()) goto exit_failure;
+				z.pop();
+				break;
+			 case '"':
+				in_String=true;
+				break;
+			}
+		}
 	}
-	if(!z.empty())
-        itsFine=false;
 
-    if(itsFine)
-         cout<<"Yes\n";
-    else
-         cout<<"No\n";
-    fs.close();
+	if(!z.empty()) goto exit_failure;
 
+exit_success:
+	fs.close();
+	cout<<"Yes\n";
 	return 0;
+exit_failure:
+	fs.close();
+	cout<<"No\n";
+	return 1;
 }
