6cb459fa9c9bd3110b6caf50d4aff976b9ac2c3c
Julio B <tzoulio13@gmail.com>
2017-01-21 02:28:28 +0200

    B final

--- B/Metaglotistes2_f.cpp
+++ B/Metaglotistes2_f.cpp
@@ -2,65 +2,76 @@
 #include <string>
 #include <random>
 /*
-S -> A|S+A
-A -> B|A*B
-B -> a|b|c
- */
+G = ({S,A,B}, {a,b,c,+,*}, P, S)
+
+P:	S -> A|S+A
+	A -> B|A*B
+	B -> a|b|c
+---------------------------------
+BNF:
+1. <έκφραση< ::= <όρος>|<έκφραση>+<όρος>
+2. <όρος> ::= <παράγοντας>|<όρος>*<παράγοντας>
+3. <πααγοντας> ::= a|b|c
+*/
 #define S_ "<έκφραση>"
 #define A_ "<όρος>"
 #define B_ "<παράγοντας>"
+
 using namespace std;
+
+string S(int step){ return step?"S+A":"A";}//choose S->A or S->S+A
+string A(int step){ return step?"A*B":"B";}
+string B(int step){ return step/2?"a":step%2?"b":"c";}
+
+string printStep(string str);
+string strReplaceAll(string str, string search, string replace);
 
 #define COLOR
 
-void printSteps(string str);
-string strReplaceAll(string str, string search, string replace);
-string S(bool step){ return step?"S+A":"A";}
-string A(bool step){ return step?"A*B":"B";}
-string B(int step){ return step/2?"a":step%2?"b":"c";}
-
-int main(int argc, char** argv){
-	string p="S";
+int main(){
+	//init random
 	random_device rd;
 	mt19937 gen(rd());
-	uniform_int_distribution<> dib(0,1);
-	uniform_int_distribution<> dit(0,2);
-	cout<<"\n";
-	printSteps(p);
-	for(int i=0; i<p.length();){
-		switch(p[i]){
+	uniform_int_distribution<> rand1(0,1);
+	uniform_int_distribution<> rand2(0,2);
+
+	cout<<"Random generation:\n\n";
+	string str="S";
+	cout<<printStep(str);
+	for(int i=0; i<str.length();){
+		switch(str[i]){
 		 case 'S':
-			p.replace(i,1, S(dib(gen)));
+			str.replace(i, 1, S(rand1(gen)) );
 			break;
 		 case 'A':
-			p.replace(i,1, A(dib(gen)));
+			str.replace(i, 1, A(rand1(gen)) );
 			break;
 		 case 'B':
-			p.replace(i,1, B(dit(gen)));
+			str.replace(i, 1, B(rand2(gen)) );
 			break;
 		 default://{a,b,c,*,+}
 			++i;
 			continue;
 		}
-		cout<<" =>\n";
-		printSteps(p);
+		cout<<" =>\n"<<printStep(str);
 	}
 	cout<<"\n\n";
 	return 0;
 }
 
-void printSteps(string str){
+//e.g if str="A*B+A" then printStep="<όρος>*<βασικό>+<όρος>"
+string printStep(string str){
 	string s = str;
 	s = strReplaceAll(s, "S", S_);
 	s = strReplaceAll(s, "A", A_);
 	s = strReplaceAll(s, "B", B_);
-#ifdef COLOR
+#ifdef COLOR  ///add red color
 	if(s.find_first_of("<")!=string::npos){
 		s.insert(s.find_first_of("<"), "\e[31m");
 		s.insert(s.find_first_of(">")+1, "\e[0m");
 	}
 #endif
-	cout<<s;
+	return s;
 }
 
 string strReplaceAll(string str, string search, string replace){
