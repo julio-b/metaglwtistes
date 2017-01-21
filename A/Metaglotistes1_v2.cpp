48b69f7c37f6df62b83d6af95f8043d46474e90f
Julio B <tzoulio13@gmail.com>
2017-01-21 12:16:58 +0200

    A 1v2 final

--- A/Metaglotistes1_v2.cpp
+++ A/Metaglotistes1_v2.cpp
@@ -1,89 +1,96 @@
-#include <stack>
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <limits>
 
 using namespace std;
 
-void printSteps(string log);
 string getBrackets(istream* in);
 
 int main(int argc, char** argv){
 	string file_name="";
 	ifstream inputfs;
 	istream* in;
-	bool p_Moves = false;
-	int errcode=0;
-	string log="";
-	stack<char> z;
-	char c;
+	bool p_Steps = false;
 
-	for(int i=1; i<argc; i++){
+	for(int i=1; i<argc; i++){ //get arguments
 		string ar = argv[i];
 		if(ar=="-h" || ar=="--help"){
 			cout<<"Usage: "<<argv[0]<<" -p [filename]\n";
 			return 0;
 		}else if(ar=="-p"){
-			p_Moves = true;
+			p_Steps = true;
 		}else if(!file_name.compare("")){
 			file_name = ar;
 		}else{
 			cout<<"Invalid argument: "<<ar<<".\n";
-			return 1;
+			return -1;
 		}
 	}
-
+	//read from file or cin
 	inputfs.open(file_name.c_str());
 	in = inputfs.is_open() ? &inputfs : &cin;
-	
 	if(in == &cin){
 		if(file_name.compare("")) cout<<file_name<<" file failed!";
 		cout<<"Type your program.. <ENTER><CTRL-D> when you're done.\n";
 	}
 
-	string actinput = getBrackets(in);
-	cout<<"\n";
+	////////ΝΑΣ///////////////////////////////
 	string stackstring = "$";
-	cout<<"Περιεχομενα στοιβας\tκατασταση\tυπολειπα συμβολα εισοδου\n";
-	int katastasi=1;
-	cout<<stackstring<<"\t\t\t"<<katastasi<<"\t\t"<<actinput<<endl;
-	while(!actinput.empty()){
-		if(actinput[0]=='('){
-			actinput.erase(0,1);
-			katastasi=2;
-			stackstring+="(";
-		}
-		else if(actinput[0]==')'){
-			if(stackstring[stackstring.size()-1]!='('){
-				cout<<"error: found ')' but stack is empty\n";
-				goto exit_failure;
-				break;
-			}
-			actinput.erase(0,1);
-			stackstring.erase(stackstring.size()-1);
-			if(stackstring[stackstring.size()-1]=='$') katastasi=1;
-		}
-		cout<<stackstring<<"\t\t\t"<<katastasi<<"\t\t"<<actinput<<endl;
-	}
-	if(stackstring[stackstring.size()-1!='$']){
-		cout<<"error: eof but stack isn't empty\n";
-		errcode=2;
-		goto exit_failure;
-	}else{
-		katastasi=3;
-		cout<<stackstring<<"\t\t\t"<<katastasi<<"\t\t"<<actinput<<endl;
-	}
-exit_success:
-	cout<<"\nYES\n";
-	goto _exit;
-exit_failure:
-	cout<<"\nNO\n";
-_exit:
-	if(inputfs.is_open()) inputfs.close();
-	return errcode;
-}
+	#define StackTop stackstring[stackstring.size()-1]
+	#define StackEmpty stackstring.compare("$")==0
+	#define StackPop stackstring.erase(stackstring.size()-1)
+	#define StackPush(C) stackstring+=( C )
 
+	string input = getBrackets(in);
+	#define input_getch input[0]; input.erase(0,1); if(input.empty()) input="ε"
+	int katastasi=1;
+	#define printStep if(p_Steps) cout<<stackstring<<"\t\t\t"<<katastasi<<"\t\t"<<input<<endl
+
+	if(p_Steps) cout<<"Περιεχομενα στοιβας\tκατασταση\tυπολειπα συμβολα εισοδου\n";
+	printStep;
+	while(!input.empty()&&input.compare("ε")){
+		char c = input_getch;
+		switch(c){
+			case '(':
+				StackPush('(');
+				katastasi=2;
+				printStep;
+				break;
+			case ')':
+				if(StackTop!='('){
+					if(p_Steps) cout<<"error: found ')' but stack is empty\n";
+					goto _exit;
+				}
+				StackPop;
+				printStep;
+				if(StackEmpty){//ε-μεταβαση
+					katastasi=1;
+					printStep;
+				}
+				break;
+		}
+	}
+	//input=="ε"
+	if(!StackEmpty){
+		if(p_Steps) cout<<"error: input ε, but stack isn't empty\n";
+	}else if(katastasi==1){//ε-μεταβαση
+		katastasi=3;
+		printStep;
+	}
+	/////////////////////////////////////////////////////
+
+_exit:
+	string result = (katastasi==3)? "YES" : "NO";
+	cout<<"\n"<<result<<"\n";
+	if(inputfs.is_open()) inputfs.close();
+	return katastasi-3;
+}///main
+
+
+//read in, till EOF
+//ignore all chars from strings
+//ignore all chars outside of strings, except brackets '(' & ')'
+//e.g.  (adqweq"we)\"()("((')'))  returns ((())
 string getBrackets(istream* in){
 	bool in_String = false;
 	bool escape_char = false;
@@ -119,26 +126,5 @@ string getBrackets(istream* in){
 			}
 		}
 	}
-	cout<<br;
 	return br;
 }
-void printSteps(string log){
-	string stackstring = "$";
-	cout<<"Περιεχομενα στοιβας\tκατασταση\tυπολειπα συμβολα εισοδου\n";
-	while(!log.empty()){
-		cout<<stackstring<<"\t\t\t"<<"A\t\t"<<log<<endl;
-		if(log[0]=='('){
-			log.erase(0,1);
-			stackstring+="I";
-		}
-		else
-			if(log[0]==')'){
-				log.erase(0,1);
-				stackstring.erase(stackstring.size()-1);
-			}
-		if(stackstring.empty())
-			break;
-	}
-	cout<<stackstring<<"\t\t\t"<<"A\t\t"<<log<<endl;
-}
-
